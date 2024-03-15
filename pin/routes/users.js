c1cf0536388544ed704b1aa01a82d424c87c3313
guru12-creator <deshmukhgaurav481@gmail.com>
2024-03-16 00:21:24 +0530

    first-commit
    nothing

--- pin/routes/users.js
+++ pin/routes/users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const plm = require("passport-local-mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/pin");
+// mongoose.connect("mongodb://process.env.MONGODB_CONNECT_URI");
 const userSchema = mongoose.Schema({
   username: String,
   name: String,
